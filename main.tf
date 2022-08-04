eef5658e114d14d3813253a243005144f2874520
bhuvanesh-s01 <110398470+bhuvanesh-s01@users.noreply.github.com>
2022-08-04 16:50:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,7 @@ resource "aws_instance" "ec2_instance" {
     ami = "ami-085284d24fe829cd0"
     instance_type = "t2.micro"
     availability_zone = "us-west-1a"
-   
-   tags = {
+tags = {
     Name = "ec2"
 } 
 }
